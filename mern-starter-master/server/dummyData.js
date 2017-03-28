d859a3566854c158681f4b513d20f5c94d9acb5e
Vandivier <vandivier_john@yahoo.com>
2017-03-28 00:14:04 -0400

    change dummy data

--- mern-starter-master/server/dummyData.js
+++ mern-starter-master/server/dummyData.js
@@ -20,22 +20,21 @@ export default function () {
       occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
       est laborum`;
 
-    const content2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+    const content2 = `Doing so will cause the pane on the right side of the window to
+      toggle. The pane is a dev tool, not part of the app. When it is hidden it will
+      expose a button which may be used to add a new post. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
       in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
       occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
       est laborum. Sed ut perspiciatis unde omnis iste natus error
       sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
       eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-      vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-      aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-      qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-      ipsum quia dolor sit amet.`;
+      vitae dicta sunt explicabo.`;
 
     const post1 = new Post({ name: 'Admin', title: 'Hello MERN', slug: 'hello-mern', cuid: 'cikqgkv4q01ck7453ualdn3hd', content: content1 });
-    const post2 = new Post({ name: 'Admin', title: 'Lorem Ipsum', slug: 'lorem-ipsum', cuid: 'cikqgkv4q01ck7453ualdn3hf', content: content2 });
+    const post2 = new Post({ name: 'Admin', title: 'Press Ctrl + H', slug: 'ctrl-h', cuid: 'cikqgkv4q01ck7453ualdn3hf', content: content2 });
 
     Post.create([post1, post2], (error) => {
       if (!error) {
