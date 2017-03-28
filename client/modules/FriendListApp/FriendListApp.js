017efc56300dcc66a0ce3b50fcbbb607c884fd68
Vandivier <vandivier_john@yahoo.com>
2017-03-28 01:13:25 -0400

    location changes for a feature-oriented architecture

--- client/modules/FriendListApp/FriendListApp.js
+++ client/modules/FriendListApp/FriendListApp.js
@@ -3,17 +3,31 @@ import styles from './FriendListApp.css';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import * as FriendsActions from '../actions/FriendsActions';
-import { FriendList, AddFriendInput } from '../components';
+import * as FriendsActions from '../../actions/FriendsActions';
+import { FriendList, AddFriendInput } from '../../components';
 
+/*
+//TODO: is the @connect pattern cool or nah?
+//  For now I say nah bc decorators require an additional babel dependency, but maybe change.
+//  Alternative state interaction pattern based on MERN seed AddFriendInput.js
+//  Note to self: I don't understand the overuse of commas inside JSON in the MERN app (eg comma after last key-val pair.)
 @connect(state => ({
   friendlist: state.friendlist
 }))
+*/
+
 export default class FriendListApp extends Component {
 
   static propTypes = {
     friendsById: PropTypes.object.isRequired,
     dispatch: PropTypes.func.isRequired
+  }
+
+  constructor (props, context) {
+    super(props, context);
+    this.state = {
+      friendlist: this.friendlist,
+    };
   }
 
   render () {
